bdad59c002afa8a7213ffce50dfbdc804ee7caee
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-05-14 14:53:55 +0100

    Add IaC for database

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {}
 
+provider "vault" {
+  address = "https://vault.reform.hmcts.net:6200"
+}
+
 # Make sure the resource group exists
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
@@ -8,6 +12,28 @@ resource "azurerm_resource_group" "rg" {
 
 locals {
   ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+
+  local_env              = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+  local_ase              = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.ase_name}"
+
+  s2s_url                = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
+
+  previewVaultName       = "${var.product}-${var.component}"
+  nonPreviewVaultName    = "${var.product}-${var.component}-${var.env}"
+  vaultName              = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName}"
+
+  db_connection_options  = "?ssl=true"
+}
+
+module "db" {
+  source              = "git@github.com:hmcts/moj-module-postgres?ref=cnp-449-tactical"
+  product             = "${var.product}-${var.component}-db"
+  location            = "${var.location_db}"
+  env                 = "${var.env}"
+  database_name       = "feature_toggle"
+  postgresql_user     = "feature_toggler"
+  sku_name            = "GP_Gen5_2"
+  sku_tier            = "GeneralPurpose"
 }
 
 module "feature-toggle-api" {
@@ -20,7 +46,61 @@ module "feature-toggle-api" {
   subscription        = "${var.subscription}"
 
   app_settings = {
+    FEATURES_DB_HOST            = "${module.db.host_name}"
+    FEATURES_DB_PORT            = "${module.db.postgresql_listen_port}"
+    FEATURES_DB_USER_NAME       = "${module.db.user_name}"
+    FEATURES_DB_PASSWORD        = "${module.db.postgresql_password}"
+    FEATURES_DB_NAME            = "${module.db.postgresql_database}"
+    FEATURES_DB_CONN_OPTIONS    = "${local.db_connection_options}"
+    FLYWAY_URL                  = "jdbc:postgresql://${module.db.host_name}:${module.db.postgresql_listen_port}/${module.db.postgresql_database}${local.db_connection_options}"
+    FLYWAY_USER                 = "${module.db.user_name}"
+    FLYWAY_PASSWORD             = "${module.db.postgresql_password}"
+    // silence the "bad implementation" logs
     LOGBACK_REQUIRE_ALERT_LEVEL = false
     LOGBACK_REQUIRE_ERROR_CODE  = false
   }
 }
+
+# region save DB details to Azure Key Vault
+module "feature-toggle-key-vault" {
+  source              = "git@github.com:hmcts/moj-module-key-vault?ref=master"
+  name                = "${local.vaultName}"
+  product             = "${var.product}"
+  env                 = "${var.env}"
+  tenant_id           = "${var.tenant_id}"
+  object_id           = "${var.jenkins_AAD_objectId}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  # dcd_cc-dev group object ID
+  product_group_object_id = "38f9dea6-e861-4a50-9e73-21e64f563537"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name      = "${var.component}-POSTGRES-USER"
+  value     = "${module.db.user_name}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name      = "${var.component}-POSTGRES-PASS"
+  value     = "${module.db.postgresql_password}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name      = "${var.component}-POSTGRES-HOST"
+  value     = "${module.db.host_name}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name      = "${var.component}-POSTGRES-PORT"
+  value     = "${module.db.postgresql_listen_port}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name      = "${var.component}-POSTGRES-DATABASE"
+  value     = "${module.db.postgresql_database}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+# endregion
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -11,10 +11,32 @@ variable "location_app" {
   default = "UK South"
 }
 
+variable "location_db" {
+  type    = "string"
+  default = "UK South"
+}
+
 variable "env" {
   type = "string"
 }
 
+variable "vault_section" {
+  type = "string"
+  description = "Name of the environment-specific section in Vault key path, i.e. secret/{vault_section}/..."
+  default = "test"
+}
+
 variable "ilbIp" {}
 
+variable "tenant_id" {}
+
+variable "client_id" {
+  description = "(Required) The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies. This is usually sourced from environment variables and not normally required to be specified."
+}
+
 variable "subscription" {}
+
+variable "jenkins_AAD_objectId" {
+  type        = "string"
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
