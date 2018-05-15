d3febf23e9b8ddb103dd902e0af1e4acb6db9400
donatas M <doncem@users.noreply.github.com>
2018-05-15 08:20:02 +0100

    Migrate FF4J DB (#6)
    * Add database configuration
    
    * Add first migration
    
    * Add no-op flyway strategy
    
    * Add IaC for database
    
    * Enable DB migrations
    
    * Fix package name
    
    * Disable flyway strategy for integration tests
    
    * Rename the db module
    
    * Pad migration script filename with zeros

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,11 @@
+output "vaultUri" {
+  value = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+
+output "vaultName" {
+  value = "${local.vaultName}"
+}
+
 output "microserviceName" {
   value = "${var.component}"
 }
