42438160dc477aac8fc0eae7681b0b3733531fde
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-05-21 08:34:47 +0100

    Include ff4j API specs

--- src/main/java/uk/gov/hmcts/reform/feature/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/feature/config/SwaggerConfiguration.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.reform.feature.config;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -9,8 +12,13 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.hmcts.reform.feature.Application;
 
+@ComponentScan(basePackages = {
+    "org.ff4j.services",
+    "org.ff4j.spring.boot.web.api.resources"
+})
 @Configuration
 @EnableSwagger2
+@Lazy
 public class SwaggerConfiguration {
 
     @Bean
@@ -18,7 +26,10 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
             .useDefaultResponseMessages(false)
             .select()
-            .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
+            .apis(Predicates.or(
+                RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"),
+                RequestHandlerSelectors.basePackage("org.ff4j.spring.boot.web.api.resources")
+            ))
             .paths(PathSelectors.any())
             .build();
     }
