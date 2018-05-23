0c04e525dc45a881ea58d7ceb8cf4703d37ddada
nitinprabhu <prabhunitin61@gmail.com>
2018-05-23 09:37:59 +0100

    Added smoke tests

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/feature/CreateFeatureToggleTest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.feature;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import uk.gov.hmcts.reform.feature.categories.SmokeTestCategory;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class CreateFeatureToggleTest extends BaseTest {
+
+    @Category(SmokeTestCategory.class)
+    @Test
+    public void should_successfully_create_feature_toggle_in_feature_store() throws IOException {
+        //Feature name should be unique in the feature store
+        String featureUuid = "smoke-test-" + UUID.randomUUID();
+
+        String createRequestBody = loadJson("feature-toggle-enabled.json");
+
+        requestSpecification()
+            .log().uri()
+            .and()
+            .body(createRequestBody.replace("{uid}", featureUuid))
+            .when()
+            .put("api/ff4j/store/features/" + featureUuid)
+            .then()
+            .statusCode(201);
+
+        //Delete the created feature
+        requestSpecification()
+            .delete("api/ff4j/store/features/" + featureUuid);
+    }
+}
---
+++ src/functionalTest/java/uk/gov/hmcts/reform/feature/categories/SmokeTestCategory.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.reform.feature.categories;
+
+/**
+ * This interface represents smoke test category.
+ */
+public interface SmokeTestCategory {
+
+}
