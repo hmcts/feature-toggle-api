9bc726034047bd0294065647d5a39a65e0bfc270
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-05-25 14:27:00 +0100

    Re-work security configuration for multiple endpoints

--- src/main/java/uk/gov/hmcts/reform/feature/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/feature/config/SecurityConfiguration.java
@@ -1,45 +1,95 @@
 package uk.gov.hmcts.reform.feature.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/", "/health", "/info").permitAll()
-            .antMatchers(HttpMethod.GET, "/api/ff4j/**").hasAuthority("READ")
-            .antMatchers(HttpMethod.OPTIONS, "/api/ff4j/**").hasAuthority("READ")
-            .antMatchers(HttpMethod.DELETE, "/api/ff4j/**").hasAuthority("WRITE")
-            .antMatchers(HttpMethod.POST, "/api/ff4j/**").hasAuthority("WRITE")
-            .antMatchers(HttpMethod.PUT, "/api/ff4j/**").hasAuthority("WRITE")
-            .antMatchers("/ff4j-web-console/**").hasRole("ADMIN")
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic()
-            .and()
-            .csrf().disable();
-    }
+public class SecurityConfiguration {
 
     // TODO replace with proper set up from configs and azure 'n' stuff
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("user")
-            .password("password")
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(getUserBuilder("user", "password")
             .roles("USER")
             .authorities("READ")
-            .and()
-            .withUser("admin")
-            .password("admin")
+            .build()
+        );
+        manager.createUser(getUserBuilder("admin", "admin")
             .roles("USER", "ADMIN")
-            .authorities("READ", "WRITE");
+            .authorities("READ", "WRITE")
+            .build()
+        );
+
+        return manager;
+    }
+
+    @Configuration
+    @Order(1)
+    public static class Ff4jWebConsoleSecurityAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .antMatcher("/ff4j-web-console/**")
+                .authorizeRequests()
+                .anyRequest().hasRole("USER")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+        }
+    }
+
+    @Configuration
+    @Order(2)
+    public static class ApiSecurityAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .antMatcher("/api/ff4j/**")
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET).hasAuthority("READ")
+                .antMatchers(HttpMethod.OPTIONS).hasAuthority("READ")
+                .antMatchers(HttpMethod.DELETE).hasAuthority("WRITE")
+                .antMatchers(HttpMethod.POST).hasAuthority("WRITE")
+                .antMatchers(HttpMethod.PUT).hasAuthority("WRITE")
+                .anyRequest().hasRole("USER")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+        }
+    }
+
+    @Configuration
+    public static class DefaultSecurityAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .authorizeRequests()
+                .antMatchers("/", "/health", "/info").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+        }
+    }
+
+    private UserBuilder getUserBuilder(String username, String password) {
+        return User.withUsername(username).password(password);
     }
 }
