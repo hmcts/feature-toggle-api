e5d6f0e3d51f22911608adb988f4002ab0ec9236
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2018-05-25 08:41:12 +0100

    Added jenkins cnp file for prod pipeline (#19)
    * Added jenkins cnp file for prod pipeline
    
    * Added deployment namespace
    
    * Updated vault name logic

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -40,3 +40,7 @@ variable "jenkins_AAD_objectId" {
   type        = "string"
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
+
+variable "deployment_namespace" {
+  default = ""
+}
