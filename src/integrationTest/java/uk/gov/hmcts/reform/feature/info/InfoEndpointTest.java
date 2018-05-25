fc2ef5ec839295c4c1bea215fc58c2606bc1c2aa
nitinprabhu <prabhunitin61@gmail.com>
2018-05-25 09:20:58 +0100

    Removed unused imports

--- src/integrationTest/java/uk/gov/hmcts/reform/feature/info/InfoEndpointTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/feature/info/InfoEndpointTest.java
@@ -7,13 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
