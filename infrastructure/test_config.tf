9e68769c80a3f8d31d772a55152678ac5a6afb20
nitinprabhu <prabhunitin61@gmail.com>
2018-06-01 14:32:10 +0100

    Added vault config to load into azure key vault

--- infrastructure/test_config.tf
+++ infrastructure/test_config.tf
@@ -21,3 +21,39 @@ data "vault_generic_secret" "test-read-user" {
 data "vault_generic_secret" "test-read-password" {
   path = "secret/${var.vault_section}/cc/ff4j/webconsole/test-read-password"
 }
+
+resource "azurerm_key_vault_secret" "test-admin-user" {
+  name      = "test-admin-user"
+  value     = "${local.test_admin_user}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "test-admin-password" {
+  name      = "test-admin-password"
+  value     = "${local.test_admin_password}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "test-editor-user" {
+  name      = "test-editor-user"
+  value     = "${local.test_editor_user}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "test-editor-password" {
+  name      = "test-editor-password"
+  value     = "${local.test_editor_password}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "test-read-user" {
+  name      = "test-read-user"
+  value     = "${local.test_read_user}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "test-read-password" {
+  name      = "test-read-password"
+  value     = "${local.test_read_password}"
+  vault_uri = "${module.feature-toggle-key-vault.key_vault_uri}"
+}
