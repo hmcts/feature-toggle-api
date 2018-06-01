080bd65f7088c308a3368fa082cc927d82f9f695
nitinprabhu <prabhunitin61@gmail.com>
2018-06-01 12:33:09 +0100

    Fixed spaces and formatting

--- src/main/resources/db/migration/V002__FF4J_user_roles.sql
+++ src/main/resources/db/migration/V002__FF4J_user_roles.sql
@@ -1,12 +1,12 @@
 CREATE TABLE USERS(
-	username VARCHAR(50) NOT NULL PRIMARY KEY,
-	password VARCHAR(100) NOT NULL,
+  username VARCHAR(50) NOT NULL PRIMARY KEY,
+  password VARCHAR(100) NOT NULL,
   enabled BOOLEAN NOT NULL
 );
 
 CREATE TABLE authorities (
-	username VARCHAR(50) NOT NULL,
-	authority VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  authority VARCHAR(50) NOT NULL,
   constraint fk_authorities_users foreign KEY(username)references USERS(username)
 );
 
