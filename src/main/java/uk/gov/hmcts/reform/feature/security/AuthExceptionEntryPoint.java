c078973a92daa66365f88e567694a838dc96aa10
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-06-07 12:49:38 +0100

    exception squash

--- src/main/java/uk/gov/hmcts/reform/feature/security/AuthExceptionEntryPoint.java
+++ src/main/java/uk/gov/hmcts/reform/feature/security/AuthExceptionEntryPoint.java
@@ -5,10 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class AuthExceptionEntryPoint implements AuthenticationEntryPoint {
 
