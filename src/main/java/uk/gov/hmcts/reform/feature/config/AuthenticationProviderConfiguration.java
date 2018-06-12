403a96e2c50d55a88ffcc876c4b1af945ae2053f
donatas M <doncem@users.noreply.github.com>
2018-06-12 15:10:29 +0100

    Update configs (#42)
    * Clear up swagger configuration
    
    * Select correct logback version after spring boot downgrade
    
    * Rename configuration files to match the pattern

--- src/main/java/uk/gov/hmcts/reform/feature/config/AuthenticationProviderConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/feature/config/AuthenticationProviderConfiguration.java
@@ -15,7 +15,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @AutoConfigureBefore({SecurityConfiguration.class})
-public class AuthenticationProviderConfig {
+public class AuthenticationProviderConfiguration {
 
     @Autowired
     private DataSource dataSource;
--- src/main/java/uk/gov/hmcts/reform/feature/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/feature/config/SwaggerConfiguration.java
@@ -1,11 +1,8 @@
 package uk.gov.hmcts.reform.feature.config;
 
-import com.google.common.base.Predicates;
-import org.ff4j.spring.boot.web.api.config.SwaggerConfig;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -13,21 +10,21 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.hmcts.reform.feature.Application;
 
-@AutoConfigureBefore(SwaggerConfig.class)
 @Configuration
 @EnableSwagger2
-@Lazy
 public class SwaggerConfiguration {
 
     @Bean
+    @Primary
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
             .useDefaultResponseMessages(false)
             .select()
-            .apis(Predicates.or(
-                RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"),
-                RequestHandlerSelectors.basePackage("org.ff4j.spring.boot.web.api.resources")
-            ))
+            .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
+            .paths(PathSelectors.any())
+            .build()
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("org.ff4j.spring.boot.web.api.resources"))
             .paths(PathSelectors.any())
             .build();
     }
