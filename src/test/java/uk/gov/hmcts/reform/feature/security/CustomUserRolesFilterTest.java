59859f076942c3b6bac95d0afa79522bb22eed3e
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-06-20 13:53:28 +0100

    Improve filter test to cover all cases

--- src/test/java/uk/gov/hmcts/reform/feature/security/CustomUserRolesFilterTest.java
+++ src/test/java/uk/gov/hmcts/reform/feature/security/CustomUserRolesFilterTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.feature.security;
 
 import com.google.common.collect.ImmutableList;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.access.intercept.RunAsUserToken;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -37,11 +39,20 @@ public class CustomUserRolesFilterTest {
         filter = new CustomUserRolesFilter("/**");
     }
 
+    @After
+    public void tearDown() {
+        SecurityContextHolder.clearContext();
+    }
+
     @Test
     public void should_return_authorisation_from_context_when_no_headers_passed()
         throws IOException, ServletException {
         // given
-        Authentication anonymous = getAnonymous();
+        Authentication anonymous = new AnonymousAuthenticationToken(
+            "anonymous",
+            User.withUsername("anonymous").password("").roles(Roles.USER).build(),
+            ImmutableList.of(new SimpleGrantedAuthority(Roles.USER))
+        );
         SecurityContextHolder.getContext().setAuthentication(anonymous);
 
         // when
@@ -55,8 +66,8 @@ public class CustomUserRolesFilterTest {
     public void should_return_authorisation_from_context_when_header_value_is_empty()
         throws IOException, ServletException {
         // given
-        Authentication anonymous = getAnonymous();
-        SecurityContextHolder.getContext().setAuthentication(anonymous);
+        Authentication testingUser = getTestingUser();
+        SecurityContextHolder.getContext().setAuthentication(testingUser);
 
         // and
         given(request.getHeader(CustomUserRolesFilter.USER_ID_HEADER)).willReturn("");
@@ -66,7 +77,7 @@ public class CustomUserRolesFilterTest {
         Authentication filteredAuth = filter.attemptAuthentication(request, response);
 
         // then
-        assertThat(filteredAuth).isEqualToComparingFieldByFieldRecursively(anonymous);
+        assertThat(filteredAuth).isEqualToComparingFieldByFieldRecursively(testingUser);
     }
 
     @Test
@@ -80,16 +91,38 @@ public class CustomUserRolesFilterTest {
 
         // then
         assertThat(filteredAuth).isInstanceOf(RunAsUserToken.class);
+        assertThat(((RunAsUserToken) filteredAuth).getOriginalAuthentication())
+            .isSameAs(AnonymousAuthenticationToken.class);
         assertThat(filteredAuth.getAuthorities())
             .extracting("authority")
             .hasSameElementsAs(ImmutableList.of("test", "beta", "ROLE_" + Roles.USER));
     }
 
-    private Authentication getAnonymous() {
-        return new AnonymousAuthenticationToken(
-            "anonymous",
-            User.withUsername("anonymous").password("").roles(Roles.USER).build(),
-            ImmutableList.of(new SimpleGrantedAuthority(Roles.USER))
+    @Test
+    public void should_return_run_as_user_token_with_correct_original_authentication()
+        throws IOException, ServletException {
+        // given
+        Authentication testingUser = getTestingUser();
+        SecurityContextHolder.getContext().setAuthentication(testingUser);
+
+        // and
+        given(request.getHeader(CustomUserRolesFilter.USER_ID_HEADER)).willReturn("id");
+        given(request.getHeader(CustomUserRolesFilter.USER_ROLES_HEADER)).willReturn("test");
+
+        // when
+        Authentication filteredAuth = filter.attemptAuthentication(request, response);
+
+        // then
+        assertThat(filteredAuth).isInstanceOf(RunAsUserToken.class);
+        assertThat(((RunAsUserToken) filteredAuth).getOriginalAuthentication()).isSameAs(testingUser.getClass());
+        assertThat(filteredAuth.getAuthorities())
+            .extracting("authority")
+            .hasSameElementsAs(ImmutableList.of("test", "ROLE_" + Roles.USER));
+    }
+
+    private Authentication getTestingUser() {
+        return new TestingAuthenticationToken(
+            User.withUsername("test").password("").authorities("test").build(), ""
         );
     }
 }
