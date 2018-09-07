5d8ae9b9e508fbd2faf509c80e4b0fbc2b48b8d6
Tim Jacomb <t.jacomb@kainos.com>
2018-09-07 13:44:25 +0100

    Revert "Remove unused vault provider (#72)" (#73)
    This reverts commit 50b19805d90205d63f32216fb795bafb61727e0a.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {}
 
+provider "vault" {
+  address = "https://vault.reform.hmcts.net:6200"
+}
+
 # Make sure the resource group exists
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
