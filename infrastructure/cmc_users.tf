cd297652ab5dc8c3baf7cc45476e3cf3832978e9
Leszek Gonczar <gonczar@gmail.com>
2018-11-01 15:37:56 +0000

    Make sure secrets are not retrieved before vault is created (#80)

--- infrastructure/cmc_users.tf
+++ infrastructure/cmc_users.tf
@@ -1,19 +1,23 @@
 data "azurerm_key_vault_secret" "admin_username_cmc" {
   name = "admin-username-cmc"
   vault_uri = "${local.permanent_vault_uri}"
+  depends_on = ["module.feature-toggle-key-vault"]
 }
 
 data "azurerm_key_vault_secret" "admin_password_cmc" {
   name = "admin-password-cmc"
   vault_uri = "${local.permanent_vault_uri}"
+  depends_on = ["module.feature-toggle-key-vault"]
 }
 
 data "azurerm_key_vault_secret" "editor_username_cmc" {
   name = "editor-username-cmc"
   vault_uri = "${local.permanent_vault_uri}"
+  depends_on = ["module.feature-toggle-key-vault"]
 }
 
 data "azurerm_key_vault_secret" "editor_password_cmc" {
   name = "editor-password-cmc"
   vault_uri = "${local.permanent_vault_uri}"
+  depends_on = ["module.feature-toggle-key-vault"]
 }
--- infrastructure/probate_users.tf
+++ infrastructure/probate_users.tf
@@ -1,19 +1,23 @@
 data "azurerm_key_vault_secret" "admin_username_probate" {
   name = "admin-username-probate"
   vault_uri = "${local.permanent_vault_uri}"
+  depends_on = ["module.feature-toggle-key-vault"]
 }
 
 data "azurerm_key_vault_secret" "admin_password_probate" {
   name = "admin-password-probate"
   vault_uri = "${local.permanent_vault_uri}"
+  depends_on = ["module.feature-toggle-key-vault"]
 }
 
 data "azurerm_key_vault_secret" "editor_username_probate" {
   name = "editor-username-probate"
   vault_uri = "${local.permanent_vault_uri}"
+  depends_on = ["module.feature-toggle-key-vault"]
 }
 
 data "azurerm_key_vault_secret" "editor_password_probate" {
   name = "editor-password-probate"
   vault_uri = "${local.permanent_vault_uri}"
+  depends_on = ["module.feature-toggle-key-vault"]
 }
