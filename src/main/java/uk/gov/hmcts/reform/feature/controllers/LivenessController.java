28c64a6733a5dfbcc50b8fe63da268be429becb7
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-02-25 22:12:29 +0000

    correcting request mapping to get mapping

--- src/main/java/uk/gov/hmcts/reform/feature/controllers/LivenessController.java
+++ src/main/java/uk/gov/hmcts/reform/feature/controllers/LivenessController.java
@@ -4,11 +4,7 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 import static org.springframework.http.ResponseEntity.ok;
 
