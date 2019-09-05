dde1147f1dea727e1e3dc70906ce51cf358c5d90
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2019-09-05 12:00:06 +0100

    Fixing dependency check issue (#124)

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,11 +1,3 @@
 output "vaultUri" {
   value = "${module.feature-toggle-key-vault.key_vault_uri}"
 }
-
-output "vaultName" {
-  value = "${local.vaultName}"
-}
-
-output "microserviceName" {
-  value = "${var.component}"
-}
