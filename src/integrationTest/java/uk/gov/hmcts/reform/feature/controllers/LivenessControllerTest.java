f998fdb331e92737f43bd983a02dfdf7c8a1d7c7
luigibk <41112628+luigibk@users.noreply.github.com>
2020-02-18 10:58:50 +0000

    Update checkstyle (#133)
    Co-authored-by: Tim Jacomb <t.jacomb@kainos.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/feature/controllers/LivenessControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/feature/controllers/LivenessControllerTest.java
@@ -1,25 +1,41 @@
 package uk.gov.hmcts.reform.feature.controllers;
 
+import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockFilterConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class LivenessControllerTest {
-    @Autowired
+
     private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    @Before
+    public void setup() {
+        WebRequestTrackingFilter filter = new WebRequestTrackingFilter();
+        filter.init(new MockFilterConfig()); // using a mock that you construct with init params and all
+        this.mvc = webAppContextSetup(this.webAppContext)
+            .addFilters(filter).build();
+    }
 
     @Test
     public void givenAppIsRunning_whenGetLiveness_thenReturnsUp()
